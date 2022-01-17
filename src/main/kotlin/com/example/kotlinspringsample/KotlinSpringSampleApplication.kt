00435b3954ca0c0b2b2a52509748346d2ba10f06
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-17 21:38:12 +0900

    :sparkles: Add Spring WebFlux

---
+++ src/main/kotlin/com/example/kotlinspringsample/KotlinSpringSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringsample/KotlinSpringSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
