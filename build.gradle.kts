865672310bb2894a93b20dcf56f518f72f6f135e
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-18 13:14:00 +0900

    :heavy_plus_sign: Add Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.1.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -27,6 +29,10 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
 }
 
 tasks.withType<KotlinCompile> {
