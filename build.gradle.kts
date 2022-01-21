2b8b2f0146b77d30583678cac924627e2da9bc24
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-21 18:44:49 +0900

    :heavy_plus_sign: Add SpringMockK

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,12 +31,16 @@ dependencies {
     implementation("com.expediagroup:graphql-kotlin-spring-server:$graphqlKotlinVersion")
     implementation("com.expediagroup:graphql-kotlin-schema-generator:$graphqlKotlinVersion")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(module = "mockito-core")
+    }
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
+    testImplementation("com.ninja-squad:springmockk:3.1.0")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
 }
 
