d7453c34c6720a573d70b54861622e85f030d4a1
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-25 17:23:23 +0900

    :white_check_mark: Add SpringMockK test

--- src/test/kotlin/com/example/kotlinspringsample/KotlinSpringSampleApplicationTests.kt
+++ src/test/kotlin/com/example/kotlinspringsample/KotlinSpringSampleApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.example.kotlinspringsample
 
-import org.junit.jupiter.api.Test
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.verify
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KotlinSpringSampleApplicationTests {
+class KotlinSpringSampleApplicationTests : DescribeSpec() {
+    @MockkBean
+    private lateinit var greetingService: GreetingService
 
-    @Test
-    @Suppress("EmptyFunctionBlock")
-    fun contextLoads() {
+    @Autowired
+    private lateinit var controller: GreetingController
+
+    init {
+        it("should greet by delegating to the greeting service") {
+            every { greetingService.greet("John") } returns "Hi John"
+
+            controller.greet("John") shouldBe "Hi John"
+            verify { greetingService.greet("John") }
+        }
     }
 }
